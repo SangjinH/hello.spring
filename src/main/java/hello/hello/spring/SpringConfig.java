9ff4c1f6ac3789f111d12111eed2166dae2137fa
SangjinH <54434944+SangjinH@users.noreply.github.com>
2020-11-16 17:37:48 +0900

    스프링 다형성을 이용한 JdbcTemplateMemberRepository이용

--- src/main/java/hello/hello/spring/SpringConfig.java
+++ src/main/java/hello/hello/spring/SpringConfig.java
@@ -1,13 +1,25 @@
 package hello.hello.spring;
 
+import hello.hello.spring.repository.JdbcMemberRepository;
+import hello.hello.spring.repository.JdbcTemplateMemberRepository;
 import hello.hello.spring.repository.MemberRepository;
 import hello.hello.spring.repository.MemoryMemberRepository;
 import hello.hello.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
